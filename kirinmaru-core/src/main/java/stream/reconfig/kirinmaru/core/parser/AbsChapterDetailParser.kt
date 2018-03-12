dcaca569237e09996398577d2bde92da3f4b3675
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-12 19:09:03 +0800

    Allow a nullable clean function in AbsChapterDetailParser

--- kirinmaru-core/src/main/java/stream/reconfig/kirinmaru/core/parser/AbsChapterDetailParser.kt
+++ kirinmaru-core/src/main/java/stream/reconfig/kirinmaru/core/parser/AbsChapterDetailParser.kt
@@ -9,15 +9,17 @@ abstract class AbsChapterDetailParser(
     open val rawText: String,
     open val nextUrl: String,
     open val prevUrl: String,
-    open inline val transformer: (rawText: String?, nextUrl: String?, prevUrl: String?) -> ChapterDetail = ::CoreChapterDetail
+    open inline val transformer: (rawText: String?, nextUrl: String?, prevUrl: String?) -> ChapterDetail = ::CoreChapterDetail,
+    open inline val clean: ((ChapterDetail) -> ChapterDetail)? = null
 ) : Parser<ChapterDetail> {
 
   override fun parse(document: Document): ChapterDetail {
-    return transformer(
+    val result = transformer(
         document.selectBy(rawText) { it.html() },
         document.selectBy(nextUrl) { it.first().attr("href") },
         document.selectBy(prevUrl) { it.first().attr("href") }
     )
+    return clean?.invoke(result) ?: result
   }
 }
 
