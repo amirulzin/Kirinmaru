db28673689a399bc5695356624034c35c2cf0222
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-15 05:19:42 +0800

    Let CurrentPref to just generate preference key via NovelId

--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/prefs/CurrentReadPref.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/prefs/CurrentReadPref.kt
@@ -1,20 +1,20 @@
 package stream.reconfig.kirinmaru.android.prefs
 
 import android.content.SharedPreferences
-import stream.reconfig.kirinmaru.android.ui.novels.NovelItem
 import stream.reconfig.kirinmaru.android.util.prefs.PrefDynamicModel
+import stream.reconfig.kirinmaru.core.NovelId
 import javax.inject.Inject
 
 /**
- * Current read chapter dynamic keyed by a [NovelItem]
+ * Current read chapter url dynamically keyed by a [NovelId]
  */
-class CurrentReadPref @Inject constructor(prefs: SharedPreferences) : PrefDynamicModel<String?, NovelItem>(
+class CurrentReadPref @Inject constructor(prefs: SharedPreferences) : PrefDynamicModel<String?, NovelId>(
     default = null,
     retrieve = { key, default -> getString(key, default) },
     store = { key, data -> putString(key, data) },
     prefs = prefs
 ) {
-  override fun key(input: NovelItem): String {
-    return "curRead_${input.id}"
+  override fun key(input: NovelId): String {
+    return "cr_${input.origin}_${input.url}"
   }
 }
