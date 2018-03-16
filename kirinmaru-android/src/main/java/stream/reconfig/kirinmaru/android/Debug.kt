f621ff3e59fe214c1e9a19f1d3a12158ce4dc738
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-17 02:51:49 +0800

    Enable temporary RxJava logging

--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/Debug.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/Debug.kt
@@ -1,9 +1,15 @@
 package stream.reconfig.kirinmaru.android
 
 import android.util.Log
+import io.reactivex.Flowable
+import io.reactivex.Single
 
 const val TEST_TAG = "TEST_TAG"
 
 fun logd(string: String) {
   Log.d(TEST_TAG, string)
 }
+
+fun <T> Flowable<T>.logOnNext(msg: (T) -> Any? = { "" }): Flowable<T> = doOnNext { logd("OnNext: ${msg.invoke(it)}") }
+
+fun <T> Single<T>.logOnSuccess(msg: (T) -> Any? = { "" }): Single<T> = doOnSuccess { logd("OnSuccess: ${msg.invoke(it)}") }
