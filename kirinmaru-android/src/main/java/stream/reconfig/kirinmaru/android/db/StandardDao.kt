fd9f5be32d5339f05ed9ad627c73f8ef401ee909
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-17 21:18:47 +0800

    Migrate StandardDao to conform SQLite behavior

--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/db/StandardDao.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/db/StandardDao.kt
@@ -3,16 +3,29 @@ package stream.reconfig.kirinmaru.android.db
 import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Update
 
 /**
  * Standard Room DAO for insert and delete
  */
 interface StandardDao<in T> {
   @Insert(onConflict = OnConflictStrategy.REPLACE)
-  fun insert(data: T): Long
+  fun insertReplace(data: T): Long
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
-  fun insert(data: List<T>): List<Long>
+  fun insertReplace(data: List<T>): List<Long>
+
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
+  fun insertIgnore(data: T): Long
+
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
+  fun insertIgnore(data: List<T>): List<Long>
+
+  @Update(onConflict = OnConflictStrategy.IGNORE)
+  fun updateIgnore(data: T): Int
+
+  @Update(onConflict = OnConflictStrategy.IGNORE)
+  fun updateIgnore(data: List<T>): Int
 
   @Delete
   fun delete(data: T)
