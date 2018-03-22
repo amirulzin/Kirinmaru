2bdc6e9b8480d04aff842fb1604bfa0003db0622
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-23 02:09:47 +0800

    Refactor ReaderSetting default creation

--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/reader/ReaderPref.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/reader/ReaderPref.kt
@@ -1,16 +1,19 @@
 package stream.reconfig.kirinmaru.android.ui.reader
 
+import android.content.Context
 import android.content.SharedPreferences
 import com.google.gson.Gson
+import stream.reconfig.kirinmaru.android.di.qualifiers.ApplicationContext
 import stream.reconfig.kirinmaru.android.util.prefs.GsonPrefModel
 import javax.inject.Inject
 
 class ReaderPref @Inject constructor(
+    @ApplicationContext context: Context,
     prefs: SharedPreferences,
     gson: Gson
 ) : GsonPrefModel<ReaderSetting>(
     key = "reader",
-    default = ReaderSetting(),
+    default = ReaderSetting.default(context),
     prefs = prefs,
     gson = gson
 )
--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/reader/ReaderSetting.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/reader/ReaderSetting.kt
@@ -1,14 +1,35 @@
 package stream.reconfig.kirinmaru.android.ui.reader
 
+import android.content.Context
+import android.support.annotation.ColorInt
+import android.support.v4.content.ContextCompat
 import stream.reconfig.kirinmaru.android.R
+import stream.reconfig.kirinmaru.android.assets.Fonts
 
 data class ReaderSetting(
-    val settingKey: String = "global", // or by novelUrl
-    val fontSizeSp: Int = 14,
-    val letterSpacingSp: Int = 0,
-    val lineSpacingExtra: Int = 0,
-    val fontName: String = "DEFAULT",
-    val fontColor: Int = R.color.colorTextPrimary,
-    val backgroundColor: Int = R.color.colorWindowBackground,
-    val isGlobal: Boolean = true
-)
+    val settingKey: String,
+    val fontSizeSp: Int,
+    val letterSpacingSp: Int,
+    val lineSpacingExtra: Int,
+    val fontName: String,
+    @ColorInt val fontColor: Int,
+    @ColorInt val backgroundColor: Int,
+    val isGlobal: Boolean
+) {
+  companion object {
+    const val KEY_GLOBAL = "global"
+
+    @JvmStatic
+    fun default(context: Context) = ReaderSetting(
+        settingKey = KEY_GLOBAL,
+        fontSizeSp = 16,
+        letterSpacingSp = 0,
+        lineSpacingExtra = 0,
+        fontName = Fonts.DEFAULT_TYPEFACE_NAME,
+        fontColor = ContextCompat.getColor(context, R.color.colorTextPrimary),
+        backgroundColor = ContextCompat.getColor(context, R.color.colorWindowBackground),
+        isGlobal = true
+    )
+  }
+}
+
