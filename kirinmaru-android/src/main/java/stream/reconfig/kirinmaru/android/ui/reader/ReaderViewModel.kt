3476f5f551fcf82fafdeec16722fc809965b7635
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-23 04:31:00 +0800

    Persist reader setting when inactive

--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/reader/ReaderViewModel.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/reader/ReaderViewModel.kt
@@ -2,7 +2,7 @@ package stream.reconfig.kirinmaru.android.ui.reader
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import io.reactivex.Completable
+import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import stream.reconfig.kirinmaru.android.util.livedata.RxMediatorLiveData
 import stream.reconfig.kirinmaru.android.util.rx.addTo
@@ -17,10 +17,16 @@ class ReaderViewModel @Inject constructor(
   val readerSetting = object : RxMediatorLiveData<ReaderSetting>() {
     override fun onActive() {
       super.onActive()
-      Completable.fromCallable { postValue(readerPref.load()) }
+      Single.fromCallable { readerPref.load() }
+          .map(::postValue)
           .subscribeOn(Schedulers.io())
           .subscribe()
           .addTo(disposables)
+    }
+
+    override fun onInactive() {
+      super.onInactive()
+      value?.let { readerPref.persist(it) }
     }
   }
 }
