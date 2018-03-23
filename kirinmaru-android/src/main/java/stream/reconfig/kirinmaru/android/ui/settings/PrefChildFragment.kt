509bd33087979315e012de79b03b6cafccd51525
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-24 04:23:00 +0800

    Implement FirstNavPref toggle in PrefChildFragment

--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/settings/PrefChildFragment.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/settings/PrefChildFragment.kt
@@ -1,14 +1,18 @@
 package stream.reconfig.kirinmaru.android.ui.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import io.reactivex.Completable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import stream.reconfig.kirinmaru.android.BuildConfig
 import stream.reconfig.kirinmaru.android.R
 import stream.reconfig.kirinmaru.android.db.Database
+import stream.reconfig.kirinmaru.android.prefs.FirstNavPref
+import stream.reconfig.kirinmaru.android.prefs.SHARED_PREF_NAME
 import stream.reconfig.kirinmaru.android.util.preference.DaggerPreferenceFragmentCompat
 import javax.inject.Inject
 
@@ -21,7 +25,14 @@ class PrefChildFragment : DaggerPreferenceFragmentCompat() {
   @Inject
   lateinit var database: Database
 
+  @Inject
+  lateinit var firstNavPref: FirstNavPref
+
   override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+    preferenceManager.apply {
+      sharedPreferencesName = SHARED_PREF_NAME
+      sharedPreferencesMode = Context.MODE_PRIVATE
+    }
     addPreferencesFromResource(R.xml.preferences)
   }
 
@@ -31,7 +42,22 @@ class PrefChildFragment : DaggerPreferenceFragmentCompat() {
   }
 
   private fun setup() {
-    findPreference("key_reset_database").setOnPreferenceClickListener {
+
+    findPreference(getString(R.string.first_nav_key)).apply {
+      Single.fromCallable { firstNavPref.load() }
+          .subscribeOn(Schedulers.io())
+          .observeOn(AndroidSchedulers.mainThread())
+          .subscribe { successId, _ ->
+            setDefaultValue(successId == R.id.navLibrary)
+            setOnPreferenceChangeListener { _, newValue ->
+              if (newValue as Boolean) firstNavPref.persist(R.id.navLibrary)
+              else firstNavPref.persist(R.id.navCatalogues)
+              true
+            }
+          }
+    }
+
+    findPreference(getString(R.string.reset_database_key)).setOnPreferenceClickListener {
       Completable.fromCallable { database.clearAllTables() }
           .subscribeOn(Schedulers.io())
           .observeOn(AndroidSchedulers.mainThread())
@@ -45,7 +71,7 @@ class PrefChildFragment : DaggerPreferenceFragmentCompat() {
       true
     }
 
-    findPreference("key_version")
+    findPreference(getString(R.string.version_key))
         .summary = BuildConfig.VERSION_NAME
   }
 
