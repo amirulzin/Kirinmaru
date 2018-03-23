b217b50d78b98ab141d35f86c09ba878b3842838
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-23 19:21:34 +0800

    Add typed TypeToken to ReaderPref

--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/reader/ReaderPref.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/reader/ReaderPref.kt
@@ -3,6 +3,7 @@ package stream.reconfig.kirinmaru.android.ui.reader
 import android.content.Context
 import android.content.SharedPreferences
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import stream.reconfig.kirinmaru.android.di.qualifiers.ApplicationContext
 import stream.reconfig.kirinmaru.android.util.prefs.GsonPrefModel
 import javax.inject.Inject
@@ -15,5 +16,6 @@ class ReaderPref @Inject constructor(
     key = "reader",
     default = ReaderSetting.default(context),
     prefs = prefs,
-    gson = gson
+    gson = gson,
+    type = lazy { object : TypeToken<ReaderSetting>() {}.type }
 )
