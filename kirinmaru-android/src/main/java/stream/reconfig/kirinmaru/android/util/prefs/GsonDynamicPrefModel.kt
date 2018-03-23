6aa183f9223fb6186b4271ea560ed902a71b0c13
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-23 19:48:28 +0800

    Import GsonDynamicPrefModel

---
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/util/prefs/GsonDynamicPrefModel.kt
@@ -0,0 +1,30 @@
+package stream.reconfig.kirinmaru.android.util.prefs
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import java.lang.reflect.Type
+
+/**
+ * PrefDynamicModel based on a Gson-friendly type [T]
+ * and String key created from a [P] parameter.
+ *
+ * Data is stored as JSON string.
+ *
+ * If [T] is nullable, set the `default` constructor parameter to null.
+ */
+abstract class GsonDynamicPrefModel<T, in P>(
+    default: T,
+    gson: Gson,
+    prefs: SharedPreferences,
+    type: Lazy<Type>
+) : PrefDynamicModel<T, P>(
+    default = default,
+    retrieve = { key, default ->
+      gson.fromJson(getString(key, null), type.value) ?: default
+    },
+    store = { key, data -> putString(key, gson.toJson(data)) },
+    prefs = prefs
+)
+
+
+
