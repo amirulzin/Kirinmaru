6440f1198446698a58f68f4d45162d492221d6ce
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-26 21:11:33 +0800

    Add LibraryCache for collecting and burst framing emissions

---
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/ui/library/LibraryCache.kt
@@ -0,0 +1,24 @@
+package stream.reconfig.kirinmaru.android.ui.library
+
+import android.arch.lifecycle.MutableLiveData
+import stream.reconfig.kirinmaru.core.NovelId
+
+/**
+ * Simple key-value cache for collecting LibraryItem emission and
+ * posting each item updates (or delta state) as a cumulative collection update
+ */
+class LibraryCache : MutableLiveData<MutableMap<NovelId, LibraryItem>>() {
+  init {
+    value = mutableMapOf()
+  }
+
+  fun update(libraryItem: LibraryItem) {
+    postValue(value().apply { put(libraryItem.novel, libraryItem) })
+  }
+
+  fun remove(libraryItem: LibraryItem) {
+    postValue(value().apply { remove(libraryItem.novel) })
+  }
+
+  private fun value() = value!!
+}
