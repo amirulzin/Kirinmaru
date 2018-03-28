6c237f36462574c13b9808f77bbcc70c30bf53a4
Amirul Zin <amirul.mdzin@gmail.com>
2018-03-28 23:45:41 +0800

    Add DBChapterId

---
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/vo/DBChapterId.kt
@@ -0,0 +1,8 @@
+package stream.reconfig.kirinmaru.android.vo
+
+import stream.reconfig.kirinmaru.core.ChapterId
+
+data class DBChapterId(
+    override val url: String,
+    override val title: String?
+) : ChapterId
