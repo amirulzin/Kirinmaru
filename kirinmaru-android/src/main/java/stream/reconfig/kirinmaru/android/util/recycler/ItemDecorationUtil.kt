236fd7cd53785d50a9821f05f8566b1167a6820d
Amirul Zin <amirul.mdzin@gmail.com>
2019-03-17 00:24:39 +0800

    Refactor ItemDecorationUtil

--- kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/util/recycler/ItemDecorationUtil.kt
+++ kirinmaru-android/src/main/java/stream/reconfig/kirinmaru/android/util/recycler/ItemDecorationUtil.kt
@@ -1,7 +1,6 @@
 package stream.reconfig.kirinmaru.android.util.recycler
 
 import android.content.res.Resources
-import com.baseconfig.pillar.utils.RecyclerViewUtils
 import stream.reconfig.kirinmaru.android.R
 
 /**
@@ -9,5 +8,5 @@ import stream.reconfig.kirinmaru.android.R
  */
 object ItemDecorationUtil {
   @JvmStatic
-  fun defaultVerticalDecor(resources: Resources) = RecyclerViewUtils.VerticalItemDecoration(resources.getDimensionPixelSize(R.dimen.recycler_item_margin))
+  fun defaultVerticalDecor(resources: Resources) = VerticalItemDecoration(resources.getDimensionPixelSize(R.dimen.recycler_item_margin))
 }
