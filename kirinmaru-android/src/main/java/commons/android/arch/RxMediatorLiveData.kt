3b42530f460070748454515f3cf54d9249c54224
Amirul Zin <amirul.mdzin@gmail.com>
2019-03-17 19:16:34 +0800

    Add ofDisposable to RxMediatorLiveData

--- kirinmaru-android/src/main/java/commons/android/arch/RxMediatorLiveData.kt
+++ kirinmaru-android/src/main/java/commons/android/arch/RxMediatorLiveData.kt
@@ -2,6 +2,7 @@ package commons.android.arch
 
 import android.arch.lifecycle.MediatorLiveData
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 /**
  * MediatorLiveData that calls [CompositeDisposable.clear] when onActive is invoked
@@ -10,4 +11,8 @@ abstract class RxMediatorLiveData<T> : MediatorLiveData<T>() {
   protected val disposables = CompositeDisposable()
 
   fun clearDisposables() = disposables.clear()
+
+  fun ofDisposable(disposable: Disposable) {
+    disposables.add(disposable)
+  }
 }
