e694a618a9d652a38359ef4419147a5792809d8d
Amirul Zin <amirul.mdzin@gmail.com>
2019-03-17 19:26:02 +0800

    Move VerticalItemDecoration to commons

--- kirinmaru-android/src/main/java/commons/android/core/recycler/VerticalItemDecoration.kt
+++ kirinmaru-android/src/main/java/commons/android/core/recycler/VerticalItemDecoration.kt
@@ -1,10 +1,11 @@
-package stream.reconfig.kirinmaru.android.util.recycler
+package commons.android.core.recycler
 
 import android.content.Context
 import android.graphics.Rect
+import android.support.annotation.DimenRes
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import stream.reconfig.kirinmaru.android.util.context.ContextUtils
+import commons.android.core.context.ContextUtils
 
 class VerticalItemDecoration : RecyclerView.ItemDecoration {
   private var pixelMargin: Int = 0
@@ -17,13 +18,14 @@ class VerticalItemDecoration : RecyclerView.ItemDecoration {
     this.pixelMargin = ContextUtils.getPixels(context, dpMargin)
   }
 
+  constructor(context: Context, @DimenRes dimenRes: Int) {
+    this.pixelMargin = context.resources.getDimensionPixelSize(dimenRes)
+  }
+
   override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-
     if (parent.getChildAdapterPosition(view) == 0) outRect.top = pixelMargin
-
     outRect.bottom = pixelMargin
     outRect.left = pixelMargin
     outRect.right = pixelMargin
-
   }
 }
