5c424faddfb2670c65a1fd5b4eaf31bad765aeef
Amirul Zin <amirul.mdzin@gmail.com>
2019-03-17 20:00:21 +0800

    Remove RxUtil due to inclusion of RxKotlin

--- kirinmaru-android/src/main/java/commons/android/rx/RxUtil.kt
+++
@@ -1,9 +0,0 @@
-package commons.android.rx
-
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-/**
- * Add this disposable to the passed CompositeDisposable
- */
-fun Disposable.addTo(c: CompositeDisposable) = c.add(this)
