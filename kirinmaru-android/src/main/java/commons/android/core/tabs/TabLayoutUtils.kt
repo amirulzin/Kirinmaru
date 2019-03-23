64e1bad65b374ed6d926fbbe4b3e3b4697d8dfb3
Amirul Zin <amirul.mdzin@gmail.com>
2019-03-23 22:18:12 +0800

    Let TabLayout.tintIcon use ContextCompat

--- kirinmaru-android/src/main/java/commons/android/core/tabs/TabLayoutUtils.kt
+++ kirinmaru-android/src/main/java/commons/android/core/tabs/TabLayoutUtils.kt
@@ -1,24 +1,19 @@
 package commons.android.core.tabs
 
-import android.os.Build
 import android.support.annotation.ColorRes
 import android.support.annotation.UiThread
 import android.support.design.widget.TabLayout
+import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
 
 @UiThread
 fun TabLayout.tintIcon(@ColorRes colorRes: Int) {
-
-  val colors = if (Build.VERSION.SDK_INT >= 23) {
-    resources.getColorStateList(colorRes, context.theme)
-  } else {
-    resources.getColorStateList(colorRes)
-  }
-
-  for (i in 0 until tabCount) {
-    getTabAt(i)?.icon?.let {
-      DrawableCompat.wrap(it)
-      DrawableCompat.setTintList(it, colors)
+  ContextCompat.getColorStateList(context, colorRes)?.let { colors ->
+    for (i in 0 until tabCount) {
+      getTabAt(i)?.icon?.let { drawable ->
+        DrawableCompat.wrap(drawable)
+        DrawableCompat.setTintList(drawable, colors)
+      }
     }
   }
 }
