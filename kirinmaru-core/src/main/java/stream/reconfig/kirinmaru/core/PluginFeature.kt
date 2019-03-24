07bb4328d6f947bcb3f15b0260dc12048e111e57
Amirul Zin <amirul.mdzin@gmail.com>
2019-03-24 14:06:49 +0800

    Add CAN_SEARCH_NOVEL to PluginFeature

--- kirinmaru-core/src/main/java/stream/reconfig/kirinmaru/core/PluginFeature.kt
+++ kirinmaru-core/src/main/java/stream/reconfig/kirinmaru/core/PluginFeature.kt
@@ -1,7 +1,11 @@
 package stream.reconfig.kirinmaru.core
 
+/**
+ * If the keys below doesn't exist when the Plugin is being used, then it default to the feature being unavailable
+ */
 enum class PluginFeature {
   COVER_IMAGE,
+  CAN_SEARCH_NOVEL,
   PAGED_SEARCH,
   PAGED_CHAPTER_IDS,
 }
